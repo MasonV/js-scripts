ec172cacaba2d3dd73799cb773d2f3bba1eace4f
MasonVerkruisen@proton.me <32044590+MasonV@users.noreply.github.com>
2026-04-16 21:21:58 -0400

    Luna Autoclaim V0.1.0 - Open each claim in a new tab

---
+++ luna-autoclaim/luna-autoclaim.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         luna autoclaim
+// @namespace    luna-autoclaim
+// @version      0.1.0
+// @description  Bulk-reveal and bulk-redeem keys on Luna
+// @match        https://luna.amazon.ca/claims/*
+// @homepageURL  https://github.com/MasonV/js-scripts
+// @supportURL   https://github.com/MasonV/js-scripts/issues
+// @updateURL    https://raw.githubusercontent.com/MasonV/js-scripts/main/fanatical-autoclaim/fanatical-autoclaim.meta.js
+// @downloadURL  https://raw.githubusercontent.com/MasonV/js-scripts/main/fanatical-autoclaim/fanatical-autoclaim.user.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_openInTab
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
